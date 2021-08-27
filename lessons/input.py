6e998dadeb1c2c5f3c04125556064ad756e1f081
Jackson Sommers <jsommers1@unc.edu>
2021-08-27 12:23:05 -0400

    The first version of the imput lesson program

---
+++ lessons/input.py
@@ -0,0 +1,7 @@
+"""Demonstrates asking the user for input."""
+
+__author__ = "730312274"
+
+user_name: str = input("what is your name? ")
+print("Hello, " + user_name + ", good morning!")
+print("You are awesome, " + user_name)
