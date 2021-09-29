6b05d9267b9de3ed1226465d0aa7d4f4a3279a9e
Jackson Sommers <jsommers1@unc.edu>
2021-09-29 15:06:44 -0400

    Cleaned up bugs

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -9,25 +9,29 @@ __author__ = "730312274"
 def all(haystack: list[int], needle: int) -> bool:
     """Determines if a list of integers are all the same integer."""
     i: int = 0
-
-    while i < len(haystack):
-        item: int = haystack[i]
-        if item != needle:
-            return False
-        i = i + 1
-    return True
+    if len(haystack) == 0:
+        return False
+    else:
+        while i < len(haystack):
+            item: int = haystack[i]
+            if item != needle:
+                return False
+            i = i + 1
+        return True
 
 
 def is_equal(l1: list[int], l2: list[int]) -> bool:
+    """Checks if two lists are exactly equal."""
     if l1 != l2:
         return False
     return True
 
 
 def max(haystack: list[int]) -> int:
+    """Finds the highest integer in a list of integers."""
     if len(haystack) == 0:
         raise ValueError("max() arg is an empty List")
-    current_high: int = 0
+    current_high: int = haystack[0]
     i: int = 0
     while i < len(haystack):
         if haystack[i] > current_high:
