c83ff3e01577659b4913579ddd1b78e2ebfd7dd3
Jackson Sommers <jsommers1@unc.edu>
2021-10-19 15:30:55 -0400

    First Versions of EX06

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,46 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730312274"
 
 # Define your functions below
+
+
+def invert(package: dict[str, str]) -> dict[str, str]:
+    """Inverts the keys and values of a dictionary of strings."""
+    output: dict[str, str] = {}
+    for key in package:
+        if f"{package[key]}" in output:
+            raise KeyError("Resultant dictionary will have duplicate keys!")
+        else:
+            output[f"{package[key]}"] = key
+    return output
+
+
+def favorite_color(package: dict[str, str]) -> str:
+    """Determines the most common value in a dictionary of strings."""
+    output: str = ""
+    count_table: dict[str, int] = {}
+    current_high: int = -1
+    for key in package:
+        if f"{package[key]}" in count_table:
+            count_table[f"{package[key]}"] += 1
+        else:
+            count_table[f"{package[key]}"] = 1
+    for key in count_table:
+        if count_table[key] > current_high:
+            output = key
+            current_high = count_table[key]
+    return output
+
+
+def count(haystack: list[str]) -> dict[str, int]:
+    """Counts the number of times an item appears in a list."""
+    output: dict[str, int] = {}
+    i = 0
+    while i < len(haystack):
+        if f"{haystack[i]}" in output:
+            output[f"{haystack[i]}"] += 1
+        else:
+            output[f"{haystack[i]}"] = 1
+        i += 1
+    return output
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -3,4 +3,84 @@
 # TODO: Uncomment the below line when ready to write unit tests
 # from exercises.ex06.dictionaries import invert, favorite_color, count
 
-__author__ = "123456789"
+__author__ = "730312274"
+
+
+import pytest
+from exercises.ex06.dictionaries import invert, favorite_color, count
+
+
+# Testing invert
+def test_invert_empty() -> None:
+    """Tests invert works on empty lest."""
+    xs: dict = {}
+    assert invert(xs) == {}
+
+
+def test_invert_letters() -> None:
+    """Tests invert works on a dictionary of letters."""
+    xs: dict = {'a': 'z', 'b': 'y', 'c': 'x'}
+    assert invert(xs) == {'z': 'a', 'y': 'b', 'x': 'c'}
+
+
+def test_invert_long_strings() -> None:
+    """Tests invert works on longer strings like words."""
+    xs: dict = {'apple': 'cat', 'crab': 'pizza'}
+    assert invert(xs) == {'cat': 'apple', 'pizza': 'crab'}
+
+
+def test_invert_result_error() -> None:
+    """Tests invery returns an error when resultant dict will have duplicate keys."""
+    with pytest.raises(KeyError):
+        my_dictionary = {'kris': 'jordan', 'michael': 'jordan'}
+        invert(my_dictionary)
+
+
+# Testing favorite_color
+def test_favorite_empty() -> None:
+    """Tests favorite_color works on empty lest."""
+    xs: dict = {}
+    assert favorite_color(xs) == ""
+
+
+def test_favorite_clear_winner() -> None:
+    """Test favorite_color when there is a clear favorite color."""
+    xs: dict = {"Marc": "yellow", "Ezri": "blue", "Kris": "blue"}
+    assert favorite_color(xs) == "blue"
+
+
+def test_favorite_tie_all() -> None:
+    """Test favorite_color returns first color mentioned when there there is a tie among two or more colors."""
+    xs: dict = {"Marc": "yellow", "Ezri": "blue", "Kris": "green"}
+    assert favorite_color(xs) == "yellow"
+
+
+def test_favorite_tie_some() -> None:
+    """Test favorite_color returns first color mentioned when there there is a tie among two or more colors that have two or more hits."""
+    xs: dict = {"Marc": "yellow", "Ezri": "blue", "Kris": "green", "Jackson": "blue", "Larry": "green"}
+    assert favorite_color(xs) == "blue"
+
+
+# Testing count
+def test_count_empty() -> None:
+    """Tests count works on empty lest."""
+    xs: list = []
+    assert count(xs) == {}
+
+
+def test_count_unique_items() -> None:
+    """Tests count can count a list of unique items."""
+    xs: list = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple"]
+    assert count(xs) == {"Red": 1, "Orange": 1, "Yellow": 1, "Green": 1, "Blue": 1, "Purple": 1}
+
+
+def test_count_some_dupes() -> None:
+    """Tests count can count up duplicate itmes."""
+    xs: list = ["One", "Two", "Two", "Three"]
+    assert count(xs) == {"One": 1, "Two": 2, "Three": 1}
+
+
+def test_count_all_same() -> None:
+    """Tests count can tally a whole list of duplicate phrases."""
+    xs: list = ["Squirrel!", "Squirrel!", "Squirrel!", "Squirrel!", "Squirrel!"]
+    assert count(xs) == {"Squirrel!": 5}
