3658141b1457b6b226c6a6cc4ca05c329310102c
Jackson Sommers <jsommers1@unc.edu>
2021-11-09 15:49:10 -0500

    fixed exercise work

--- exercises/ex07/data_utils.py
+++ exercises/ex07/data_utils.py
@@ -9,7 +9,7 @@ from csv import DictReader
 
 
 def read_csv_rows(filename: str) -> list[dict[str, str]]:
-    """Read rows of a scv into a table."""
+    """Read rows of a csv into a table."""
     result: list[dict[str, str]] = []
     file_handle = open(filename, "r", encoding="utf8")
     csv_reader = DictReader(file_handle)
@@ -40,18 +40,13 @@ def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
 def head(full_table: dict[str, list[str]], result_size: int) -> dict[str, list[str]]:
     """Shows a specified number of rows from a column-based table."""
     result: dict[str, list[str]] = {}
-    key_list: list[str] = []
     for key in full_table:
-        key_list.append(key)
-    first_row: list[str] = full_table[key_list[0]]
-    for column in first_row:
-        i: int = 0
         holding_list: list[str] = []
+        i = 0
         while i < result_size:
-            item: str = column[i]
-            holding_list.append(item)
+            holding_list.append(full_table[key][i])
             i += 1
-        result[column] = holding_list
+        result[key] = holding_list
     return result
 
 
@@ -59,14 +54,13 @@ def select(full_table: dict[str, list[str]], specific_columns: list[str]) -> dic
     """Makes a new table of rows from a column-oriented table based on a list of row names."""
     result: dict[str, list[str]] = {}
     key_list: list[str] = []
+    i = 0
     for key in full_table:
         key_list.append(key)
-    for item in specific_columns:
-        i: int = 0
-        while i < len(key_list):
-            if item == key_list[i]:
-                result[f"{key_list[i]}"] = full_table[key_list[i]]
-            i += 1
+    while i < len(key_list):
+        if key_list[i] in specific_columns:
+            result[key_list[i]] = full_table[key_list[i]]
+        i += 1
     return result
 
 
@@ -86,9 +80,9 @@ def concat(l1: dict[str, list[str]], l2: dict[str, list[str]]) -> dict[str, list
     n: int = 0
     while n < len(l2):
         if f"{key_list2[n]}" in result:
-            result[f"{key_list2[n]}"] = l1[f"{key_list1[n]}"] + l2[f"{key_list1[n]}"]
+            result[f"{key_list2[n]}"] = l1[f"{key_list2[n]}"] + l2[f"{key_list2[n]}"]
         else:
-            result[f"{key_list2[n]}"] = l2[f"{key_list1[n]}"]
+            result[f"{key_list2[n]}"] = l2[f"{key_list2[n]}"]
         n += 1
     return result
 
